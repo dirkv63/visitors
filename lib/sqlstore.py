4b844c7c0dacb4a15bc3f4212018edbefc248bf4
Dirk Vermeylen <dirk.vermeylen@skynet.be>
2018-11-03 16:48:49 +0100

    Logfile processing

--- lib/sqlstore.py
+++ lib/sqlstore.py
@@ -46,6 +46,20 @@ class UserAgent(Base):
     mobile = Column(Integer)
 
 
+class FileHash(Base):
+    """
+    Table containing the file hashes.This is used to check if there is an update on the logfile.
+    """
+    __tablename__ = "filehash"
+    file_id = Column(Text, primary_key=True)
+    fh = Column(Text, nullable=False)
+    created = Column(Integer, nullable=False)
+    modified = Column(Integer, nullable=False)
+
+    def __repr__(self):
+        return "<File: {fid} - hash: {h}>".format(fid=self.file_id, h=self.fh)
+
+
 class DirectConn:
     """
     This class will set up a direct connection to the database. It allows to reset the database,
@@ -75,49 +89,11 @@ class DirectConn:
         logging.debug("Datastore object and cursor are created")
         return
 
-    def db_commit(self):
-        """
-        This method will commit open actions as required.
-        :return:
-        """
-        self.dbConn.commit()
-        return
-
-    def get_query(self, query):
-        """
-        This method will get a query and return the result of the query.
-        :param query:
-        :return:
-        """
-        self.cur.execute(query)
-        res = self.cur.fetchall()
-        return res
-
-    def get_table(self, tablename):
-        """
-        This method will return the table as a list of named rows. This means that each row in the list will return
-        the table column value as an attribute. E.g. row.name will return the value for column name in each row.
-        :param tablename:
-        :return:
-        """
-        query = "SELECT * FROM {t}".format(t=tablename)
-        self.cur.execute(query)
-        res = self.cur.fetchall()
-        return res
-
-    def insert_row(self, tablename, rowdict):
-        columns = ", ".join(rowdict.keys())
-        values_template = ", ".join(["?"] * len(rowdict.keys()))
-        query = "insert into {tn} ({cols}) values ({vt})".format(tn=tablename, cols=columns, vt=values_template)
-        values = tuple(rowdict[key] for key in rowdict.keys())
-        self.cur.execute(query, values)
-        return self.cur.lastrowid
-
     def rebuild(self):
         # A drop for sqlite is a remove of the file
+        db_name = os.path.basename(self.db)
         try:
             os.remove(self.db)
-            db_name = os.path.basename(self.db)
             logging.info("Database {db} will be recreated".format(db=db_name))
         except FileNotFoundError:
             logging.info("New database {db} will be created".format(db=db_name))
@@ -127,41 +103,6 @@ class DirectConn:
         conn_string = "sqlite:///{db}".format(db=self.db)
         engine = set_engine(conn_string=conn_string)
         Base.metadata.create_all(engine)
-
-    def run_query(self, query):
-        """
-        This method will run a query and not return any value. This can be used for CREATE TABLE or DELETE statements
-        :param query:
-        :return:
-        """
-        self.cur.execute(query)
-        return
-
-    def update_row(self, tn, rowdict):
-        """
-        This method will update the record with ID rowdict["id"] with the values for columns as specified in dictionary
-        rowdict.
-        :param tn: tablename
-        :param rowdict: "id" field is mandatory
-        :return:
-        """
-        try:
-            rec_id = rowdict.pop("id")
-        except KeyError:
-            logging.error("Trying to update table {tn} with {d}, but record ID is missing.".format(tn=tn, d=rowdict))
-            return False
-        else:
-            setarr = ["{k}=?".format(k=k) for k in rowdict]
-            settemp = ", ".join(setarr)
-            vals = [rowdict[k] for k in rowdict]
-            vals.append(rec_id)
-            query = "update {tn} set {settemp} where id=?".format(tn=tn, settemp=settemp)
-            try:
-                self.cur.execute(query, tuple(vals))
-                return self.cur.lastrowid
-            except sqlite3.InterfaceError:
-                logging.error("Something bad happened with query {q} and values {v}".format(q=query, v=vals))
-                return  False
 
 
 def init_session(db, echo=False):
--- parse_log.py
+++ parse_log.py
@@ -1,27 +1,75 @@
 """
-This script will parse the apache log file and convert each line in an action record and a user agent record. This
-script will handle all new records from the logfile.
+This script will parse the apache log file and convert each line in an action record and a user agent record. The script
+will process logfile other_vhosts_access.log.1. Each time a change in the file is detected the assumption is that this
+is a new file and every line in the file is processed.
 """
+
 import apache_log_parser
+import hashlib
 import logging
+import time
 from lib import my_env
 from lib import sqlstore
-from lib.sqlstore import Request, UserAgent
+from lib.sqlstore import Request, UserAgent, FileHash
+from sqlalchemy.orm.exc import NoResultFound
+
+
+def file_update(file_id, fc):
+    """
+    This method will check if the file is updated since last run.
+    :param file_id: Unique identifier for the file, use filename.
+    :param fc: Contents (slurp) of the file.
+    :return: True if the file was updated, False if there is no change.
+    """
+    # Get the hash for the file
+    m = hashlib.sha256()
+    m.update(fc)
+    fh = m.hexdigest()
+    # Check if file is changed since last run
+    try:
+        fr = sess.query(FileHash).filter_by(file_id=file_id).one()
+    except NoResultFound:
+        # New file, add to database
+        frec = FileHash(
+            file_id=file_id,
+            fh=fh,
+            created=int(time.time()),
+            modified=int(time.time())
+        )
+        sess.add(frec)
+        sess.commit()
+        return True
+    # Existing file, check if there was an update
+    if fr.fh == fh:
+        # Same checksum, no need to update
+        return False
+    else:
+        fr.fh = fh
+        fr.modified = int(time.time())
+        sess.commit()
+        return True
+
 
 cfg = my_env.init_env("visitors", __file__)
 logging.info("Start application")
 sess = sqlstore.init_session(cfg["Main"]["db"])
+logfile = cfg["Main"]["apache_log"]
+# Slurp logfile to calculate checksum.
+with open(logfile, 'rb') as fp:
+    if not file_update("other_vhosts", fp.read()):
+        raise SystemExit(0)
+
+# logfile other_vhosts_access_log.1 has been updated, load all records in database.
 res = sess.query(UserAgent).all()
 ua_dict = {}
 for rec in res:
     ua_dict[rec.desc] = rec.id
-logfile = "data/other_vhosts_access.log.1"
 parse_str = "%v:%p %h %l %u %t \"%r\" %>s %O \"%{Referer}i\" \"%{User-Agent}i\""
 line_parser = apache_log_parser.make_parser(parse_str)
 cnt = 0
+# Now open logfile and handle line by line.
 with open(logfile, 'r') as file:
     line = file.readline()
-    li = my_env.LoopInfo("logrecords", 100)
     while line:
         line_data = line_parser(line)
         desc = line_data["request_header_user_agent"]
@@ -53,5 +101,3 @@ with open(logfile, 'r') as file:
         sess.add(request)
         sess.commit()
         line = file.readline()
-        li.info_loop()
-    li.end_loop()
